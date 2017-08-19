b03e31da32b4ece020257f02afd0c3c064dd2a69
Hao-Yi Wu <haoyi.wu@gmail.com>
2017-08-19 12:27:48 -0500

    Fist commit.

--- Home_files/Home.js
+++ Home_files/Home.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20161231
+// Created by iWeb 3.0.4 local-build-20170819
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Home_files/Watercolor_iweb_01.png'),IWCreateImage('Home_files/Watercolor_iweb_02.png'),IWCreateImage('Home_files/Watercolor_iweb_04.png'),IWCreateImage('Home_files/Watercolor_iweb_08.png'),IWCreateImage('Home_files/Watercolor_iweb_16.png'),IWCreateImage('Home_files/Watercolor_iweb_14.png'),IWCreateImage('Home_files/Watercolor_iweb_13.png'),IWCreateImage('Home_files/Watercolor_iweb_05.png')],null,0,0.800000,0.000000,0.000000,0.000000,0.000000,12.000000,12.000000,11.000000,11.000000,13.000000,12.000000,13.000000,12.000000,null,null,null,0.500000)});registry.applyEffects();}
--- Publications_files/Publications.js
+++ Publications_files/Publications.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20161231
+// Created by iWeb 3.0.4 local-build-20170819
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
